f86eeda33d314e79edc7a668aa2c65d5fb2681ec
vishnuvardhan1685 <vishnuvardhan1685@gmail.com>
2026-06-17 00:55:28 +0530

    feat.implemented the config files of db, redis, cloudinary and model creation of User,Resume & Job

--- server/src/models/Job.js
+++ server/src/models/Job.js
@@ -2,3 +2,18 @@
 // _id, userId (ObjectId ref 'User'), title, company,
 // seniorityLevel, jdText, extractedSkills ([String]),
 // pgvectorId (Number — row id from pgvector), createdAt
+import mongoose from 'mongoose';
+
+const jobSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    seniorityLevel: { type: String },
+    jdText: { type: String },
+    extractedSkills: [{ type: String }],
+    pgvectorId: { type: Number },
+    createdAt: { type: Date, default: Date.now },
+})
+
+export default mongoose.model('Job', jobSchema);
